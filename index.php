497cd3a0419a5a5c616630471eeb8c97a05a0178
Purnama Indrayuni_2201010578 <115921079+Yuni31@users.noreply.github.com>
2023-03-14 17:03:27 +0800

    SESI 1

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hari Pertama Kelas</title>
+</head>
+<body>
+
+    <h3>Belajar Web Programming<?="<sup>80</sup>"?></h3>
+    <p>Mari Belajar Bersama</p>
+
+    <script>
+        document.write("<h3>Menggunakan Script javascript</h3>");
+    </script>
+
+<?php
+    echo "<h3>Menggunakan Script PHP</h3>";
+
+?>
+
+    
+</body>
+</html>
